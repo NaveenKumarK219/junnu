65e502ceffc711b32427477fc29e875e35b53503
Navin Kumar <gangadari.naveen@gmail.com>
2019-06-24 15:39:29 +0530

    Push to Git

---
+++ src/main/java/com/navin/junnu/JunnuApplication.java
@@ -0,0 +1,13 @@
+package com.navin.junnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunnuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunnuApplication.class, args);
+	}
+
+}
